13e7d78b266cf8fedcad93920cbdf201c480f82c
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-08-16 21:45:30 +0300

    completed project

---
+++ H4X0R-SwiftUI/Views/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  H4X0R-SwiftUI
+//
+//  Created by ALEKSEY SUSLOV on 16.08.2022.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com/")
+    }
+}
