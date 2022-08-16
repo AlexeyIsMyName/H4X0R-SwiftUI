0c9dd63fe946385ae404753554dcd5b150db2319
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-08-16 18:51:41 +0300

    made NetworkManager

---
+++ H4X0R-SwiftUI/Models/PostData.swift
@@ -0,0 +1,23 @@
+//
+//  PostData.swift
+//  H4X0R-SwiftUI
+//
+//  Created by ALEKSEY SUSLOV on 16.08.2022.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String
+}
