4b49eaef87ae6a7c5988bd5f8843b55f820d6297
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-08-16 17:14:58 +0300

    Initial Commit

---
+++ H4X0R-SwiftUI/H4X0R_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_SwiftUIApp.swift
+//  H4X0R-SwiftUI
+//
+//  Created by ALEKSEY SUSLOV on 16.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
